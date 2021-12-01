145522552e211e0dae31ebd8afa7c5accc7a44f9
Hikachhu <florian.morin@gmail.com>
2021-12-01 17:17:57 +0100

    Partie 7 - Encore plus de composition

--- src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
+++ src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
@@ -1,12 +1,16 @@
 package fr.lernejo.guessgame;
 
-public class ComputerPlayer implements Player{
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
 
+public class ComputerPlayer implements Player{
+    private final Logger logger = LoggerFactory.getLogger("player");
     long borneDroite= Long.MAX_VALUE;
     long borneGauche=Long.MIN_VALUE;
 
     @Override
     public long askNextGuess() {
+        logger.log("Le bot envoi : "+(Long)((borneDroite+borneGauche)/2));
         return (Long)((borneDroite+borneGauche)/2);
     }
 
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -5,7 +5,15 @@ package fr.lernejo.logger;
 public class LoggerFactory{
 
     public static Logger getLogger(String name) {
-        CompositeLogger a =new CompositeLogger(new ConsoleLogger(),new FileLogger("fichier.txt"));
+
+        ContextualLogger a = new ContextualLogger( name,new CompositeLogger(
+            new ConsoleLogger(),
+            new FilteredLogger(
+                new FileLogger("fichier.txt"),
+                (message2) -> message2.contains("simulation")
+            )
+        ));
+
         return a;
     }
 
