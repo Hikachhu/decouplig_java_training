413f6b25b6cec1a466450c7fafd3e13b59135f68
Hikachhu <florian.morin@gmail.com>
2021-12-01 16:38:45 +0100

    Partie 5 - Simplification des messages de log

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,9 +1,12 @@
 package fr.lernejo.logger;
 
+
+
 public class LoggerFactory{
 
     public static Logger getLogger(String name) {
-        return new ConsoleLogger();
+        ContextualLogger a =new ContextualLogger(name,new FileLogger("fichier.txt"));
+        return a;
     }
 
 }
